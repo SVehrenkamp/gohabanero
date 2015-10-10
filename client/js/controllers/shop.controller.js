be197e7a3f557cf3d8f4b2478b43e5fe753f7177
Scott <scott.vehrenkamp@target.com>
2015-10-10 16:22:39 -0500

    Updates Shoping page UI

--- client/js/controllers/shop.controller.js
+++ client/js/controllers/shop.controller.js
@@ -2,13 +2,16 @@ angular.module('ShopCTRL', [])
 .controller('ShopCTRL', function($rootScope, $scope, $http, $cart){
 	$scope.sauces = [];
 
+
 	//Get All Sauces
 	$http.get('js/data/sauce.json').success(function(data){
 		$scope.sauces = data;
 	});
 
+
 	$scope.add_to_cart = function(index){
 		var item = $scope.sauces[index];
+		$scope.sauces[index].addedToCart = true;
 		$cart.add_item(item);
 		console.log(parseInt($cart.get_items()[0].price) * 1);
 
